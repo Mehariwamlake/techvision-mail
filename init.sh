f31bf2f40a3f23c0d04d3b312af079d9edbba417
mehairwamlake <mehariwamlake@gmail.com>
2026-04-19 19:33:16 +0300

    l

--- init.sh
+++ init.sh
@@ -13,7 +13,7 @@ bench set-redis-cache-host redis-cache:6379
 bench set-redis-queue-host redis-queue:6379
 bench set-redis-socketio-host redis-socketio:6379
 
-bench get-app mail
+bench get-app https://github.com/frappe/mail
 
 bench new-site mail.techvision.edu.et \
   --mariadb-root-password $DB_ROOT_PASSWORD \
