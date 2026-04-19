902c3e8a845fc3269c40e0f691cdc95611b5a8df
mehairwamlake <mehariwamlake@gmail.com>
2026-04-19 20:18:52 +0300

    l

--- init.sh
+++ init.sh
@@ -8,17 +8,19 @@ fi
 
 cd frappe-bench
 
+# IMPORTANT: ensure variables include redis://
 bench set-mariadb-host mariadb
-bench set-redis-cache-host $REDIS_CACHE
-bench set-redis-queue-host $REDIS_QUEUE
-bench set-redis-socketio-host $REDIS_SOCKETIO
+bench set-redis-cache-host "$REDIS_CACHE"
+bench set-redis-queue-host "$REDIS_QUEUE"
+bench set-redis-socketio-host "$REDIS_SOCKETIO"
 
 bench get-app https://github.com/frappe/mail
 
 bench new-site mail.techvision.edu.et \
-  --mariadb-root-password $DB_ROOT_PASSWORD \
-  --admin-password $ADMIN_PASSWORD \
+  --mariadb-root-password "$DB_ROOT_PASSWORD" \
+  --admin-password "$ADMIN_PASSWORD" \
   --install-app mail
+
 bench use mail.techvision.edu.et
 
 bench --site mail.techvision.edu.et set-config developer_mode 0
